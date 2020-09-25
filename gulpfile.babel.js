ee6325d413b01a7345ec9465d1b8fefcc5cbfe34
ChincharovPC <57862647+c2pc@users.noreply.github.com>
2020-09-25 18:21:53 +0300

    add animation-button component

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,73 +2,73 @@
 
 import gulp from "gulp";
 
-const requireDir = require("require-dir"),
-    paths = {
-        views: {
-            src: [
-                "./src/views/**/*.html",
-                "./src/views/pages/*.html"
-            ],
-            docs: "./docs/",
-            watch: [
-                "./src/blocks/**/*.html",
-                "./src/views/**/*.html"
-            ]
-        },
-        styles: {
-            src: "./src/styles/main.{scss,sass}",
-            docs: "./docs/styles/",
-            watch: [
-                "./src/blocks/**/*.{scss,sass}",
-                "./src/styles/**/*.{scss,sass}"
-            ]
-        },
-        scripts: {
-            src: "./src/js/index.js",
-            docs: "./docs/js/",
-            watch: [
-                "./src/blocks/**/*.js",
-                "./src/js/**/*.js"
-            ]
-        },
-        images: {
-            src: [
-                "./src/img/**/*.{jpg,jpeg,png,gif,tiff,svg}",
-                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
-            ],
-            docs: "./docs/img/",
-            watch: "./src/img/**/*.{jpg,jpeg,png,gif,svg,tiff}"
-        },
-        webp: {
-            src: [
-                "./src/img/**/*.{jpg,jpeg,png,tiff}",
-                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
-            ],
-            docs: "./docs/img/",
-            watch: [
-                "./src/img/**/*.{jpg,jpeg,png,tiff}",
-                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
-            ]
-        },
-        sprites: {
-            src: "./src/img/svg/*.svg",
-            docs: "./docs/img/sprites/",
-            watch: "./src/img/svg/*.svg"
-        },
-        fonts: {
-            src: "./src/fonts/**/*.*",
-            docs: "./docs/fonts/",
-            watch: "./src/fonts/**/*.*"
-        },
-        favicons: {
-            src: "./src/img/favicon/*.{jpg,jpeg,png,gif}",
-            docs: "./docs/img/favicons/",
-        },
-        gzip: {
-            src: "./src/.htaccess",
-            docs: "./docs/"
-        }
-    };
+const requireDir = require("require-dir");
+const paths = {
+    views: {
+        src: [
+            "./src/views/**/*.html",
+            "./src/views/pages/*.html"
+        ],
+        docs: "./docs/",
+        watch: [
+            "./src/blocks/**/*.html",
+            "./src/views/**/*.html"
+        ]
+    },
+    styles: {
+        src: "./src/styles/main.{scss,sass}",
+        docs: "./docs/styles/",
+        watch: [
+            "./src/blocks/**/*.{scss,sass}",
+            "./src/styles/**/*.{scss,sass}"
+        ]
+    },
+    scripts: {
+        src: "./src/js/index.js",
+        docs: "./docs/js/",
+        watch: [
+            "./src/blocks/**/*.js",
+            "./src/js/**/*.js"
+        ]
+    },
+    images: {
+        src: [
+            "./src/img/**/*.{jpg,jpeg,png,gif,tiff,svg}",
+            "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+        ],
+        docs: "./docs/img/",
+        watch: "./src/img/**/*.{jpg,jpeg,png,gif,svg,tiff}"
+    },
+    webp: {
+        src: [
+            "./src/img/**/*.{jpg,jpeg,png,tiff}",
+            "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+        ],
+        docs: "./docs/img/",
+        watch: [
+            "./src/img/**/*.{jpg,jpeg,png,tiff}",
+            "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+        ]
+    },
+    sprites: {
+        src: "./src/img/svg/*.svg",
+        docs: "./docs/img/sprites/",
+        watch: "./src/img/svg/*.svg"
+    },
+    fonts: {
+        src: "./src/fonts/**/*.*",
+        docs: "./docs/fonts/",
+        watch: "./src/fonts/**/*.*"
+    },
+    favicons: {
+        src: "./src/img/favicon/*.{jpg,jpeg,png,gif}",
+        docs: "./docs/img/favicons/",
+    },
+    gzip: {
+        src: "./src/.htaccess",
+        docs: "./docs/"
+    }
+};
 
 requireDir("./gulp-tasks/");
 
