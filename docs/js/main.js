7183b6c4226defbe7ac92e52890b1ed16cdcd47e
itop55 <bachida.imihi@gmail.com>
2020-09-28 09:38:10 +0300

    add logotip

--- docs/js/main.js
+++ docs/js/main.js
@@ -108,7 +108,23 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// Show hide menu in mobile version
+// Yandex.Metrika counter
+(function (m, e, t, r, i, k, a) {
+  m[i] = m[i] || function () {
+    (m[i].a = m[i].a || []).push(arguments);
+  };
+
+  m[i].l = 1 * new Date();
+  k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(67714420, "init", {
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true,
+  webvisor: true
+}); // Show hide menu in mobile version
+
 function toggleMainMenu() {
   var bodyElem = document.querySelector('body');
   var navBtn = bodyElem.querySelector('.sidebar-nav-btn');
